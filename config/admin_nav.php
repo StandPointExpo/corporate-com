80b4f2b5a913bd7fd6db3512bf4321ac95bc34a3
Alex <alex.alex.kosh@gmail.com>
2020-02-05 13:24:00 +0200

    added admin menu config file added InterventionImageCache package added imagecache config file added Thumb class for resize images vie imagecache route

---
+++ config/admin_nav.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+    [
+        'title' => 'Portfolios',
+        'icon'  => 'fa fa-list-alt',
+        'route' => 'admin.portfolios.index',
+        'children' => [],
+    ],
+    [
+        'title' => 'Pages',
+        'icon'  => 'fa fa-columns',
+        'route' => 'admin.pages.index',
+        'children' => [],
+    ],
+    [
+        'title' => 'Partners',
+        'icon'  => 'fa fa-handshake',
+        'route' => 'admin.partners.index',
+        'children' => [],
+    ],
+    [
+        'title' => 'Contacts',
+        'icon'  => 'fa fa-address-card',
+        'route' => 'admin.contacts.index',
+        'children' => [],
+    ],
+];
