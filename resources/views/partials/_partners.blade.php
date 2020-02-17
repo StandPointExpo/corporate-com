c636d0d080515e9354b562b3869c2d1e84fff69f
Drammer <drammer.g@gmail.com>
2020-02-17 11:13:47 +0200

    Remove octanorm partner

--- resources/views/partials/_partners.blade.php
+++ resources/views/partials/_partners.blade.php
@@ -3,8 +3,8 @@
 {{--    <a href="{{ $partner->link }}">    {{ $partner->name }}</a>--}}
 {{--@endforeach--}}
 
-<div class="row row-cols-1 row-cols-md-3 row-cols-lg-4 row-cols-sm-2">
-        <div class="col-md-3 col-sm-3 my-2">
+<div class="row row-cols-1 row-cols-md-3 row-cols-lg-3 row-cols-sm-3">
+        <div class="col-md-4 col-sm-4 my-2">
             <a href="http://www.ufi.org/" target="_blank">
                 <div class="box-partner-wrap box-1">
                     <span class="wrapp-img-partner">
@@ -14,7 +14,7 @@
                 </div>
             </a>
         </div>
-        <div class="col-md-3 col-sm-3 my-2">
+        <div class="col-md-4 col-sm-4 my-2">
             <a href="http://www.ucci.org.ua/" target="_blank">
                 <div class="box-partner-wrap box-2">
                     <span class="wrapp-img-partner">
@@ -24,22 +24,12 @@
                 </div>
             </a>
         </div>
-        <div class="col-md-3 col-sm-3 my-2">
+        <div class="col-md-4 col-sm-4 my-2">
             <a href="http://www.iec-expo.com.ua/" target="_blank">
                 <div class="box-partner-wrap box-3">
                     <span class="wrapp-img-partner">
                         <img src="/images/partners/partner-1-0.jpg" class="img-partner-gray">
                         <img src="/images/partners/partner-1.jpg" class="img-partner-color">
-                    </span>
-                </div>
-            </a>
-        </div>
-        <div class="col-md-3 col-sm-3 my-2">
-            <a href="http://www.octanorm.com/" target="_blank">
-                <div class="box-partner-wrap box-4">
-                    <span class="wrapp-img-partner">
-                        <img src="/images/partners/partner-2-0.jpg" class="img-partner-gray">
-                        <img src="/images/partners/partner-21.jpg" class="img-partner-color">
                     </span>
                 </div>
             </a>
