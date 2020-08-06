1e10dfd37604a7f40e1d097c773318803980144e
0lexa <alex.alex.kosh@gmail.com>
2020-08-06 16:38:57 +0300

    added TaskTechnicalParameterValueSeeder for TaskTechnicalParameterPodiumValue && TaskTechnicalParameterLogoValue.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call(TaskConstructionParameterValueSeeder::class);
+        $this->call(TaskTechnicalParameterValueSeeder::class);
         $this->call(UserTableSeeder::class);
         $this->call(LanguageTableSeeder::class);
         $this->call(PagesSeeder::class);
