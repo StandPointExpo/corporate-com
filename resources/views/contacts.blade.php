b656b05438646a4224d9374fda493a5ed3df82eb
Yurii <drammer.g@gmail.com>
2022-10-04 13:21:26 +0300

    Edit contacts

--- resources/views/contacts.blade.php
+++ resources/views/contacts.blade.php
@@ -34,7 +34,7 @@
     </div>
 </div>
 <div class="container-fluid big-map-block">
-    <div class="row">
+    <div class="row" style="min-height: 300px">
 {{--        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2540.4212339474397!2d30.589044181022913!3d50.45188022439202!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40d4cfeeb7684bed%3A0xd63c2daa1ff5f52c!2z0JHRgNC-0LLQsNGA0YHRjNC60LjQuSDQv9GA0L7RgdC_0LXQutGCLCAxNSwg0JrQuNGX0LIsIDAyMDAw!5e0!3m2!1suk!2sua!4v1581785114098!5m2!1suk!2sua" width="100%" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>--}}
 
     </div>
